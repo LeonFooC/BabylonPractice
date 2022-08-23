2ab89013c953dccd62776d5c6a4b3bb21a945b0b
Leon <leon.foo@singaporetech.edu.sg>
2022-08-23 11:19:38 +0800

    w

--- docs/ts-npm/webpack.config.js
+++ docs/ts-npm/webpack.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:768a5b1db9823e2f256c95c16a4849eaac87ecc3e0f6fe060e96784781ecc4aa
-size 877
+oid sha256:c3fed1161ee992fc852064cdf757898c81fb90f3e9bc2ee499492aa8660e539a
+size 662
